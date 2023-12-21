f60554fde7d73f5787570ad4baa3501b27f2e16e
Thurcavalcante <arthurcavalcantethur@gmail.com>
2023-12-21 15:30:29 -0300

    Puxando ajustes

--- index.py
+++ index.py
@@ -1,37 +1,48 @@
 import streamlit as st
-
 import pandas as pd
+import plotly.express as px
 
-st.set_page_config(page_title="Atividade final")
+st.set_page_config(layout="wide", page_title="Insuficiência Cardíaca")
 
-with st.container():
+# Com uma visão mensal
+#faturamento por unidade… 
+# tipo de produto mais vendido, contribuição por filial,
+#Desempenho das forma de pagamento…
+#Como estão as avaliações das filiais?
 
-    st.subheader("Meu primeiro site com o Streamlit")
+df = pd.read_csv("supermarket_sales.csv", sep=";", decimal=",")
+df["Date"] = pd.to_datetime(df["Date"])
+df=df.sort_values("Date")
 
-    st.title("Dashboard de Contratos")
+df["Month"] = df["Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
+month = st.sidebar.selectbox("Mês", df["Month"].unique())
 
-    st.write("Informações sobre os contratos fechados pela Hash&Co ao longo de maio")
+df_filtered = df[df["Month"] == month]
 
-    st.write("Quer aprender Python? [Clique aqui](https://www.hashtagtreinamentos.com/curso-python)")
+col1, col2 = st.columns(2)
+col3, col4, col5 = st.columns(3)
 
-@st.cache_data
+fig_date = px.bar(df_filtered, x="Date", y="Total", color="City", title="Faturamento por dia")
+col1.plotly_chart(fig_date, use_container_width=True)
 
-def carregar_dados():
+fig_prod = px.bar(df_filtered, x="Date", y="Product line", 
+                  color="City", title="Faturamento por tipo de produto",
+                  orientation="h")
+col2.plotly_chart(fig_prod, use_container_width=True)
 
-    tabela = pd.read_csv("resultados.csv")
 
-    return tabela
+city_total = df_filtered.groupby("City")[["Total"]].sum().reset_index()
+fig_city = px.bar(city_total, x="City", y="Total",
+                   title="Faturamento por filial")
+col3.plotly_chart(fig_city, use_container_width=True)
 
-with st.container():
 
-    st.write("---")
+fig_kind = px.pie(df_filtered, values="Total", names="Payment",
+                   title="Faturamento por tipo de pagamento")
+col4.plotly_chart(fig_kind, use_container_width=True)
 
-    qtde_dias = st.selectbox("Selecione o período", ["7D", "15D", "21D", "30D"])
 
-    num_dias = int(qtde_dias.replace("D", ""))
-
-    dados = carregar_dados()
-
-    dados = dados[-num_dias:]
-
-    st.area_chart(dados, x="Data", y="Contratos")
+city_total = df_filtered.groupby("City")[["Rating"]].mean().reset_index()
+fig_rating = px.bar(df_filtered, y="Rating", x="City",
+                   title="Avaliação")
+col5.plotly_chart(fig_rating, use_container_width=True)
